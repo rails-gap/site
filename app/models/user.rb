ddcf96711bed6646ad5e338ad35abc4bcd37a8b1
Andres Hernandez <ahernandez@growthaccelerationpartners.com>
2016-01-30 00:05:24 -0600

    Add base project

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  validates :email, format: {
+    with: /[A-Z0-9a-z]+@growthaccelerationpartners\.com/,
+    message: "Please login with your GAP account"
+  }
+
+  def self.from_omniauth(auth)
+    if auth.info.email.include?('@growthaccelerationpartners.com')
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider =  auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.name = auth.info.name
+        user.password =  Devise.friendly_token[0,20]
+      end
+    end
+  end
+
+end
